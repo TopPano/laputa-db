f53e2e194052354a2e769bf7d24654f6b99bbc6b
Ubuntu <uniray7@gmail.com>
2016-06-03 08:22:55 +0000

    fix the json files path for mongoimport

--- docker/push_mongo_data.sh
+++ docker/push_mongo_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mongod &
-mongoimport -h localhost:27017 --db verpix-dev-db --collection post --type json --file ~/laputa-schema/data.d/post.json
-mongoimport -h localhost:27017 --db verpix-dev-db --collection follow --type json --file ~/laputa-schema/data.d/follow.json
-mongoimport -h localhost:27017 --db verpix-dev-db --collection user --type json --file ~/laputa-schema/data.d/user.json
-mongoimport -h localhost:27017 --db verpix-dev-db --collection userIdentity --type json --file ~/laputa-schema/data.d/userIdentity.json
-mongoimport -h localhost:27017 --db verpix-dev-db --collection like --type json --file ~/laputa-schema/data.d/like.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection post --type json --file ~/laputa-schema/data.d/mongodb/post.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection follow --type json --file ~/laputa-schema/data.d/mongodb/follow.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection user --type json --file ~/laputa-schema/data.d/mongodb/user.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection userIdentity --type json --file ~/laputa-schema/data.d/mongodb/userIdentity.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection like --type json --file ~/laputa-schema/data.d/mongodb/like.json
 kill `pidof mongod`
