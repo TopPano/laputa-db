5d1045102fad5f7ae49840d4e8c13987465e4ee3
root <root@ip-172-31-19-102.ap-northeast-1.compute.internal>
2016-06-13 08:15:04 +0000

    add docker-compose.yml and add verpixer in user.json and add webui_mongodb

--- docker/README.md
+++ docker/README.md
@@ -1,12 +1,34 @@
 # laputa-db docker setup
 
 ### Build mongodb image:
+* If you want only build mongodb image:
 ```sh
-cd mongodb
+cd laputa-db/docker/mongodb
 docker build -t verpix-dev/mongodb .
 ```
 
-### Build webui_mongodb image:
+### Setup the whole db system(mongodb & mongo-express):
+* If you want to run mongodb and mongodb webui:
+```sh
+cd laputa-db/docker
+docker-compose up
+```
 
+* If you want run the system in daemon
+```sh
+cd laputa-db/docker
+docker-compose up -d
+```
 
-### docker compose
+* Look out the logs of each service(verpix-dev-webui-mongodb or verpix-dev-mongodb)
+```sh
+cd laputa-db/docker
+docker-compose logs verpix-dev-webui-mongodb
+docker-compose logs verpix-dev-mongodb
+```
+
+* Stop the system
+```sh
+cd laputa-db/docker
+docker-compose stop
+```
---
+++ docker/webui_mongodb/setup.py
@@ -0,0 +1,33 @@
+import os
+import re
+import socket
+import subprocess
+
+webui_mongo_config = os.environ['WEBUI_MONGO']+'/config.js'
+
+db_name = 'verpix-dev-db'
+db_ip = 'verpix-dev-mongodb'
+db_port = 27017
+user = 'user'
+passwd = 'pass'
+webui_ip = socket.gethostbyname(socket.gethostname())
+
+config_file = open(webui_mongo_config, 'r+')
+text = config_file.read()
+text = re.sub("\'db\'", "\'"+db_name+"\'", text)
+text = re.sub("\'localhost\'", "\'"+db_ip+"\'", text)
+text = re.sub('27017', str(db_port), text)
+text = re.sub("\'admin\'", "\'"+user+"\'", text)
+text = re.sub("\'pass\'", "\'"+passwd+"\'", text)
+
+config_file.seek(0)
+config_file.write(text)
+config_file.close()
+
+os.environ['VCAP_APP_HOST'] = webui_ip
+setup_cmd = ['node', os.environ['WEBUI_MONGO']+'/app.js']
+
+while True:
+    p = subprocess.Popen(setup_cmd, stderr=subprocess.PIPE)
+    err = p.communicate()
+    print(err)
