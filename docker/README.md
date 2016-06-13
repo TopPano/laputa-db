a1675e0f1a7bf643c2c279a8de0236fbe38a9a45
root <root@ip-172-31-19-102.ap-northeast-1.compute.internal>
2016-06-13 04:12:07 +0000

    finish mongodb docker file

---
+++ docker/README.md
@@ -0,0 +1,12 @@
+# laputa-db docker setup
+
+### Build mongodb image:
+```sh
+cd mongodb
+docker build -t verpix-dev/mongodb .
+```
+
+### Build webui_mongodb image:
+
+
+### docker compose
---
+++ docker/mongodb/push_mongo_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mongod &
+mongoimport -h localhost:27017 --db verpix-dev-db --collection post --type json --file /app/data.d/mongodb/post.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection follow --type json --file /app/data.d/mongodb/follow.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection user --type json --file /app/data.d/mongodb/user.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection userIdentity --type json --file /app/data.d/mongodb/userIdentity.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection like --type json --file /app/data.d/mongodb/like.json
+sleep 5
+kill `pidof mongod`
