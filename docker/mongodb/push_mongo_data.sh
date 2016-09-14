d265af9f22c0754a896851622e4654910eb83df3
uniray7 <uniray7@gmail.com>
2016-09-14 11:48:31 +0800

    fix the push sample db data script for schema changing

--- docker/mongodb/push_mongo_data.sh
+++ docker/mongodb/push_mongo_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mongod --fork --smallfiles --logpath /var/log/mongodb/mongo_init.log
-mongoimport -h localhost:27017 --db verpix-dev-db --collection post --type json --file /app/data.d/mongodb/post.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection media --type json --file /app/data.d/mongodb/media.json
 mongoimport -h localhost:27017 --db verpix-dev-db --collection follow --type json --file /app/data.d/mongodb/follow.json
 mongoimport -h localhost:27017 --db verpix-dev-db --collection user --type json --file /app/data.d/mongodb/user.json
 mongoimport -h localhost:27017 --db verpix-dev-db --collection userIdentity --type json --file /app/data.d/mongodb/userIdentity.json
