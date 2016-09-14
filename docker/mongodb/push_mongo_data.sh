e6809ae6f7236d3f3d4b705fb6182723b9f17c2b
root <uniray7@gmail.com>
2016-09-14 13:15:15 +0800

    fix bugs from media.json

--- docker/mongodb/push_mongo_data.sh
+++ docker/mongodb/push_mongo_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mongod --fork --smallfiles --logpath /var/log/mongodb/mongo_init.log
-mongoimport -h localhost:27017 --db verpix-dev-db --collection media --type json --file /app/data.d/mongodb/media.json
-mongoimport -h localhost:27017 --db verpix-dev-db --collection follow --type json --file /app/data.d/mongodb/follow.json
-mongoimport -h localhost:27017 --db verpix-dev-db --collection user --type json --file /app/data.d/mongodb/user.json
-mongoimport -h localhost:27017 --db verpix-dev-db --collection userIdentity --type json --file /app/data.d/mongodb/userIdentity.json
-mongoimport -h localhost:27017 --db verpix-dev-db --collection like --type json --file /app/data.d/mongodb/like.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection media --type json --file /app/data.d/media.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection follow --type json --file /app/data.d/follow.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection user --type json --file /app/data.d/user.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection userIdentity --type json --file /app/data.d/userIdentity.json
+mongoimport -h localhost:27017 --db verpix-dev-db --collection like --type json --file /app/data.d/like.json
 sleep 5
 kill `pidof mongod`
